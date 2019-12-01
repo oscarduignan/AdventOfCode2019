2017bb93dd6da99c6fe824ee0046d42e1d1c0813
oscar duignan <oscarduignan@gmail.com>
2019-12-01 21:27:19 +0000

    Solutions for day 1

--- day1.ex
+++ day1.ex
@@ -1,17 +1,44 @@
 defmodule Day1 do
-end
+  def mass2fuel(mass) do
+    fuel =
+      mass
+      |> Integer.floor_div(3)
+      |> Kernel.-(2)
 
-case System.argv() do
-  [] ->
-    ExUnit.start()
-
-    defmodule Day1Test do
-      use ExUnit.Case
-
-      import Day1
-
-      test "Part 1" do
-        assert true
-      end
+    if fuel <= 0 do
+      0
+    else
+      fuel + mass2fuel(fuel)
     end
+  end
 end
+
+"inputs/day1"
+|> File.read!()
+|> String.trim()
+|> String.split("\n")
+|> Enum.map(fn mass ->
+  mass
+  |> String.to_integer()
+  |> Integer.floor_div(3)
+  |> Kernel.-(2)
+end)
+|> Enum.sum()
+|> IO.inspect(label: "Part 1")
+
+"inputs/day1"
+|> File.read!()
+|> String.trim()
+|> String.split("\n")
+|> Enum.map(fn mass ->
+  mass
+  |> String.to_integer()
+  |> Day1.mass2fuel()
+end)
+|> Enum.sum()
+|> IO.inspect(label: "Part 2")
+
+# we know should need 50346 fuel
+100_756
+|> Day1.mass2fuel()
+|> IO.inspect(label: "100756")
